80082486293e1040ecc84fb9084da45a657282ca
vagrant <vagrant@precise32.(none)>
2014-02-06 18:53:08 +0000

    putting it together lab

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', {
+		'name': 'Doug Engelbart',
+		'description': 'Has awesome pet mice',
+		'imageURL': 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Douglas_Engelbart_in_2008.jpg/972px-Douglas_Engelbart_in_2008.jpg'			
+	});
 };
